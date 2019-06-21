98beb40231495d9be22a72202b4a01432dd314bc
Mizi Khanapi <mizikhanapi@gmail.com>
2019-06-21 13:23:02 +0800

    article card updated

--- wp-content/themes/mizikhanapi/template-parts/components-cards.php
+++ wp-content/themes/mizikhanapi/template-parts/components-cards.php
@@ -1,16 +1,9 @@
 <?php $category = get_the_category(); ?>
 <div class="col-md-4 article-col <?php echo $category[0]->slug; ?>">
-    <div class="article-card">
-        <?php if ( has_term('podcast','post-type' ) ) {?>
-        <a href="" class="badge">Podcast</a>
-        <?php }?>
-        <div class="image">
-            <?php 
+<?php 
 				$featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full');
-			?>
-            <a href="<?php the_permalink(); ?>" style="background-image: url(<?php echo $featured_img_url; ?>);"><img
-                    src="<?php echo $featured_img_url; ?>" alt="" /></a>
-        </div>
+			?>    
+<div class="article-card" style="background-image:url('<?php echo $featured_img_url; ?>');">
         <div class="detail">
             <!-- <ul class="meta caption grey">
                 <li class="date"><?php// echo get_the_date('d.m.Y'); ?></li>
